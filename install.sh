5fc946b90d4d38360ca5d4ac4e76acab6b857a24
crimist <crimist@protonmail.com>
2021-12-30 04:23:42 -0800

    rename favicon file in installer

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo unlink /var/www/html/index.html
 sudo unlink /var/www/html/robots.txt
-sudo unlink /var/www/html/crimist-transparent.png
+sudo unlink /var/www/html/crimist-icon.png
 
 sudo ln -v index.html /var/www/html/
 sudo ln -v robots.txt /var/www/html/
-sudo ln -v crimist-transparent.png /var/www/html/
+sudo ln -v crimist-icon.png /var/www/html/
