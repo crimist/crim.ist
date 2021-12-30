7353f3adb41cd385eef9794588dfd43a01cbe67a
crimist <crimist@protonmail.com>
2021-12-30 02:32:45 -0800

    add unlink to installer, set x bit on install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+sudo unlink index.html
+sudo unlink robots.txt
+sudo unlink crimist-transparent.png
+
 sudo ln -v index.html /var/www/html/
 sudo ln -v robots.txt /var/www/html/
 sudo ln -v crimist-transparent.png /var/www/html/
