89fd723f9095d6316c09c817ff59dcfc33ffb320
crimist <crimist@protonmail.com>
2022-01-22 20:45:14 -0800

    optimize mobile responsiveness, serve consolas, rework install script

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
-sudo unlink /var/www/html/index.html
-sudo unlink /var/www/html/robots.txt
-sudo unlink /var/www/html/crimist-icon.png
-
-sudo ln -v index.html /var/www/html/
-sudo ln -v robots.txt /var/www/html/
-sudo ln -v crimist-icon.png /var/www/html/
+foreach item (index.html robots.txt crimist-icon.png consola.ttf)
+    sudo unlink /var/www/html/$item
+    sudo ln -v $item /var/www/html/
+end
